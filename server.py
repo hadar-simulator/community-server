d40ec9084793e0a9d3fd421a6ff27a62a9429cb7
François Jolain <4466185+FrancoisJ@users.noreply.github.com>
2020-03-16 17:33:42 +0100

    dockerize app

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
+import os
 import pickle
-import sys
 
 from flask import Flask, request, abort
 from hadar.solver.study import solve
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@application.route("/", methods=['POST'])
 def compute():
+    token = os.environ.get('ACCESS_TOKEN', None)
     if token is not None and request.args['token'] != token:
         abort(403, 'Wrong access token given')
 
+    print('Receive study...', end=' ')
     study = pickle.loads(request.data)
     res = solve(study)
+
+    print('return result')
     return pickle.dumps(res)
 
 
 if __name__ == '__main__':
-    token = sys.argv[0] if len(sys.argv) > 1 else None
-    app.run(debug=False, host='0.0.0.0')
+    application.run(debug=False, host='0.0.0.0')
